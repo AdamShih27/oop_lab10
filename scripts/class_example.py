1c986b5e61667c079e72ce1bbef2137e5fa4afb6
AdamShih27 <scy.en13@nycu.edu.tw>
2025-04-21 15:06:39 +0800

    modify class_example.py

--- scripts/class_example.py
+++ scripts/class_example.py
@@ -1,31 +1,23 @@
-class animal():
-    def __init__(self, name='animal', age=0):
-        self.name = name
-        self.age = age
-    def __str__(self):
-        return f"{self.name} and {self.age} years old."
-    def speak(self):
-        print("I'm", self)
+class A:
+    def f(self): print("A.f")
 
-class dog(animal):
-    def __init__(self, name='dog', age=0):
-        super().__init__(name, age)
-    def speak(self):
-        print("WOFF!! I'm", self)
-
-class karen(dog, animal):
-    def __init__(self, name='karen', age=48):
-        super().__init__(name, age)
-    def speak(self):
-        animal.speak(self)
-    def shout2ppl(self):
-        super().speak()
-
+class B(A):
+    def f(self):
+        print("B 前")
+        super().f()    # 不是「同上」，而是去呼叫 A.f
+        print("B 後")
+class C(B):
+    def f(self):
+        print("C 前")
+        super().f()    # 動態地，下一層是 B.f，再往下又會是 A.f
+        print("C 後")
 if __name__ == "__main__":
-    a = animal()
-    a.speak()
-    d = dog()
-    d.speak()
-    m = karen()
-    m.speak()
-    m.shout2ppl()
+    c = C()
+    c.f()
+    print("=================================")
+    b = B()
+    b.f()
+    print("=================================")
+    a = A()
+    a.f()
+    print("=================================")
